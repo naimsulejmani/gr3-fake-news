4193fed6120f92ccbd741b3da347181422ef44f5
Naim Sulejmani <naim.sulejmani@gmail.com>
2025-01-08 18:44:09 +0100

    Javascript

---
+++ HELP.md
@@ -0,0 +1,34 @@
+# Getting Started
+
+### Reference Documentation
+
+1. Maven dependencies
+2. Compose.yml -> configurimin e databases
+3. application.properties
+4. Models/Entities
+5. Repositories
+6. DTOs
+7. Mappers
+8. Services
+9. Controllers or RestControllers
+10HTML+Thymeleaf or HTML + Javascript + Thymeleaf
+
+
+### Guides
+
+* When working with RestController, test it with Postman/RestClient, and then implement the front-end javascript class to handle rest api call
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/main/resources/static/js/news/app.js
+++ src/main/resources/static/js/news/app.js
@@ -1,38 +1,81 @@
 class NewsApp {
     constructor() {
         this.newsApi = new NewsApi();
-        this.inputTitle = document.getElementById("title");
-        this.buttonShowNews = document.getElementById("showNews")
-        this.buttonDeleteNews = document.getElementById("deleteNews")
+        this.newsListDiv = document.getElementById("newsList");
+        this.loaderDiv = document.getElementById("loader");
+        this.news = [];
+        this.body = document.querySelector("body");
+        this.btnRefresh = document.getElementById("btnRefresh");
+
     }
 
     async init() {
-        this.inputTitle.focus();
+        this.loadNews();
+        this.newsListDiv.addEventListener("click", async (event) => {
 
-        this.buttonShowNews.addEventListener("click", async() => {
-            if(!this.inputTitle.value)
-                await this.loadNews();
-            else {
-                const news = await this.newsApi.getNewsById(this.inputTitle.value);
-                console.log(news);
+            if (event.target.tagName === 'BUTTON' && event.target.innerText === 'Delete') {
+
+                if (!confirm("Are you sure you want to delete this news?")) {
+                    return;
+                }
+
+                console.log(event);
+                console.log(event.target);
+                console.log(event.target.dataset);
+                const {id, archived} = event.target.dataset; //destructuring
+
+                await this.newsApi.deleteNewsById(id);
+                this.loadNews();
+
+
             }
         })
 
-        this.buttonDeleteNews.addEventListener("click", async() => {
-            if(!this.inputTitle.value)
-                alert("Please enter a title to delete");
-            else {
-                const news = await this.newsApi.deleteNewsById(this.inputTitle.value);
-                console.log(news);
-            }
-        });
-
+        this.btnRefresh.addEventListener("click", async(event)=>{
+            this.loadNews();
+        })
     }
 
     async loadNews() {
-        const news = await this.newsApi.getNews();
-        console.log(news);
+        this.newsListDiv.style.display = "none";
+        this.loaderDiv.style = {display: "block"}; //"display: 'block'"
+
+        this.news = await this.newsApi.getNews();
+
+        this.displayNews();
+        this.loaderDiv.style.display = "none";
+        this.newsListDiv.style.display = "block";
+
+
     }
+
+    displayNews() {
+        this.newsListDiv.innerHTML = "";
+
+        console.log(this.news)
+        if (!this.news.length) {
+            this.newsListDiv.innerHTML = `<h1>There are no news</h1>`;
+        }
+
+        this.news.forEach(news => {
+            this.newsListDiv.innerHTML += `
+         <div class="col-sm-6 mb-3 mb-sm-0">
+            <div class="card">
+                <div class="card-body">
+                   
+                    <h5 class="card-title">#${news.id} - ${news.title}</h5>
+                    <p class="card-text">${news.description.substring(0, 100) + '...'}</p>
+                    <a href="${news.url}" class="btn btn-primary">Go to article news</a>
+                    <button type="button" class="btn btn-warning" style="float:right; margin-right: 5px;">Edit</button>
+                    <button type="button" class="btn btn-danger" style="float:right;margin-right: 5px;"
+                    data-id="${news.id}" data-archived="${news.archived}">Delete</button>
+                </div>
+            </div>
+        </div>
+            `;
+        });
+    }
+
 }
 
 // new NewsApp().init();
